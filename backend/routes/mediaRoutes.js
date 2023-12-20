37a5eb0ceaa245c40bbfb3ddc620a2b0d9f9c7c3
EvelynDelCarmen <hey@evelyndelcarmen.com>
2023-12-20 20:23:45 +0100

    added the mediaRoutes.js

---
+++ backend/routes/mediaRoutes.js
@@ -0,0 +1,23 @@
+import express from "express";
+import { UserModel } from "../models/UserModel";
+const router = express.Router();
+
+router.get("/user/media", async (req, res) => {
+    const userId = req.user._id;
+
+    try {
+        const user = await UserModel.findById(userId);
+
+        if (!user) {
+            return res.status(404).json({ success: false, response: "User not found" });
+        }
+
+        const { imagePaths, filmPaths } = user;
+
+        res.status(200).json({ success: true, response: { imagePaths, filmPaths } });
+    } catch (error) {
+        res.status(500).json({ success: false, response: error.message });
+    }
+});
+
+export default router;
