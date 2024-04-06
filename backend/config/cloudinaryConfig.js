3ef8a4f1562c078ecb19fad4510ea577c0dc34dc
EvelynDelCarmen <hey@evelyndelcarmen.com>
2024-04-06 23:03:27 +0200

    added the react_app

--- backend/config/cloudinaryConfig.js
+++ backend/config/cloudinaryConfig.js
@@ -4,9 +4,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+    cloud_name: process.env.REACT_APP_CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.REACT_APP_CLOUDINARY_API_KEY,
+    api_secret: process.env.REACT_APP_CLOUDINARY_API_SECRET
 });
 
 export default cloudinary;
