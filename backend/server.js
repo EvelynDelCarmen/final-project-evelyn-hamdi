17df46495bb89b4f6dd54c2dd55302c80c5975f0
EvelynDelCarmen <hey@evelyndelcarmen.com>
2024-04-22 17:37:04 +0200

    changed back the CORS code

--- backend/server.js
+++ backend/server.js
@@ -3,43 +3,27 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import expressListEndpoints from "express-list-endpoints";
-dotenv.config();
 import userRoutes from "./routes/userRoutes.js";
 import questionRoutes from "./routes/questionRoutes.js"
 import mediaRoutes from "./routes/mediaRoutes.js"
 import { connectDB } from "./config/db.js";
 
-
-const port = process.env.PORT;
+dotenv.config();
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.all('/', function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
-
-app.get('/', function (req, res, next) {
-  // Handle the get for this route
-});
-
-app.post('/', function (req, res, next) {
-  // Handle the post for this route
-});
 
 
 app.use(cors()); // Enable CORS (Cross-Origin Resource Sharing)
 app.use(express.json()); // Parse incoming JSON data
-app.use(express.urlencoded({ extended: false })); // Parse URL-encoded data
+// app.use(express.urlencoded({ extended: false })); // Parse URL-encoded data
+app.use(express.urlencoded({ extended: true }));
 
+connectDB();
 
 app.use(userRoutes);
 app.use(questionRoutes);
 app.use(mediaRoutes);
-
-
-
-connectDB();
 
 
 
@@ -50,8 +34,10 @@ app.get("/", (req, res) => {
   console.log(endpoints);
 });
 
+// Global error handler
 app.use((err, req, res, next) => {
-  res.status(500).send(err);
+  console.error(`Error: ${err.message}`);
+  res.status(500).send('Server Error');
 });
 
 
