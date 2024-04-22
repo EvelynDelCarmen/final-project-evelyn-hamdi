8fd529eab3bfb38e2d0569ccb8eff5fe2a467dda
EvelynDelCarmen <hey@evelyndelcarmen.com>
2024-04-22 17:02:36 +0200

    added the app.all code

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,20 @@ import { connectDB } from "./config/db.js";
 const port = process.env.PORT;
 const app = express();
 
+app.all('/', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+app.get('/', function (req, res, next) {
+  // Handle the get for this route
+});
+
+app.post('/', function (req, res, next) {
+  // Handle the post for this route
+});
+
 
 app.use(cors()); // Enable CORS (Cross-Origin Resource Sharing)
 app.use(express.json()); // Parse incoming JSON data
@@ -26,6 +40,8 @@ app.use(mediaRoutes);
 
 
 connectDB();
+
+
 
 app.get("/", (req, res) => {
   const endpoints = expressListEndpoints(app);
