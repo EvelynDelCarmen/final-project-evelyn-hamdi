5727a0c0a5085aade2f2e52c8d0c0d29b1bcec7f
EvelynDelCarmen <hey@evelyndelcarmen.com>
2024-04-23 17:26:53 +0200

    updated CORS with credentials

--- backend/server.js
+++ backend/server.js
@@ -12,42 +12,20 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
-
 // app.use(cors()); // Enable CORS (Cross-Origin Resource Sharing)
 
-// Apply CORS to all responses
-// app.use(cors({
-//   origin: ['http://localhost:5174'],  // Add more origins as needed
-//   credentials: true,  // If your frontend needs to send cookies or authorization headers
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
-// }));
+app.use(cors({
+  origin: "http://localhost:5173", // or your frontend's deployed URL - HÄR MÅSTE JAG LÄGGA IN MIN FRONTEND URL NÄR DEN ÄR DEPLOYAD
+  credentials: true,
+}));
 
-// Log the response headers
-// app.use((req, res, next) => {
-//   res.on('finish', () => {
-//     console.log('Response headers:', res.getHeaders());
-//   });
-//   next();
-// });
 
 app.use(express.json()); // Parse incoming JSON data
 app.use(express.urlencoded({ extended: false })); // Parse URL-encoded data
 // app.use(express.urlencoded({ extended: true }));
 
-// This is often handled by the cors() middleware itself, but you can also add this if needed:
-// app.options('*', cors()); // Enable preflight for all routes
-
-
 
 connectDB();
-
-
-
 
 app.use(userRoutes);
 app.use(questionRoutes);
@@ -67,13 +45,6 @@ app.use((err, req, res, next) => {
   console.error(`Error: ${err.message}`);
   res.status(500).send('Server Error');
 });
-
-// Example of simple request logging
-// app.use((req, res, next) => {
-//   console.log(`Incoming request: ${req.method} ${req.path}`);
-//   next();
-// });
-
 
 
 app.listen(port, () => {
