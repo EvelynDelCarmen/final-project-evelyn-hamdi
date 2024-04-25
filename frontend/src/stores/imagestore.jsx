ddc1fe71772fb1ff4f8bf8615c2b6144b85d7c75
EvelynDelCarmen <hey@evelyndelcarmen.com>
2024-04-25 12:25:08 +0200

    cleaned the imagestore code

--- frontend/src/stores/imagestore.jsx
+++ frontend/src/stores/imagestore.jsx
@@ -1,37 +1,3 @@
-// import { create } from 'zustand';
-// import axios from 'axios';
-
-// const useImageStore = create((set) => ({
-//     images: [],
-//     isLoading: false,
-//     error: null,
-//     fetchImages: async (folderName) => {
-//         set({ isLoading: true, error: null });
-//         const url = `${import.meta.env.VITE_BACKEND_URL}/media`; // Construct the URL
-//         console.log("Attempting to fetch from URL:", url); // Log the URL to console
-
-//         try {
-//             const response = await axios.get(url, {
-//                 params: { folderName: folderName }
-//             });
-//             console.log('Images received:', response.data.images);
-//             set({ images: response.data.images, isLoading: false });
-//         } catch (error) {
-//             console.error('Error fetching images:', error);
-//             set({ isLoading: false, error: error.message });
-//         }
-//     },
-// }));
-
-// export default useImageStore;
-
-
-
-
-
-
-
-
 
 import { create } from 'zustand';
 import axios from 'axios';
