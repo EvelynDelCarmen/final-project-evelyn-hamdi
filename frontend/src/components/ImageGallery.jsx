9dd113776cf9499c5391abf284a8cc433a8f06a2
EvelynDelCarmen <hey@evelyndelcarmen.com>
2024-04-27 22:36:32 +0200

    folders inside next

--- frontend/src/components/ImageGallery.jsx
+++ frontend/src/components/ImageGallery.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import Masonry from 'masonry-layout';
 import useImageStore from '../stores/imagestore';
 
 const ImageGallery = () => {
@@ -24,23 +23,6 @@ const ImageGallery = () => {
 
         // cover 
         return (
-            // <div className="folder-grid p-4 bg-black text-white">
-            //     {folders.map((folder) => (
-            //         <div key={folder.path} className="folder-cover" onClick={() => navigate(`/gallery/${folder.path}`)}>
-            //             <img src={folder.coverImage} alt={`Cover for ${folder.name}`} className="cover-image" />
-            //             {/* <p>{folder.name}</p> */}
-            //         </div>
-            //     ))}
-            // </div>
-
-            // <div className="folder-grid grid grid-cols-3 gap-4 p-4">
-            //     {folders.map((folder) => (
-            //         <div key={folder.path} className="folder-cover cursor-pointer shadow-lg hover:shadow-xl rounded-lg overflow-hidden transition-shadow duration-300" onClick={() => navigate(`/gallery/${folder.path}`)}>
-            //             <img src={folder.coverImage} alt={`Cover for ${folder.name}`} className="cover-image w-full h-full object-cover" />
-            //             <p className="text-center text-white bg-black bg-opacity-75 p-2">{folder.name}</p>
-            //         </div>
-            //     ))}
-            // </div>
 
             <div className="folder-grid">
                 {folders.map((folder) => (
@@ -58,10 +40,12 @@ const ImageGallery = () => {
     }
     //folders inside
     return (
-        <div className="masonry p-4 bg-black text-white">
+        <div className="image-grid p-4 bg-black text-white">
             {images.map((image) => (
-                <div key={image.public_id} className="masonry-image overflow-hidden rounded-lg mb-4">
-                    <img src={image.url} alt="Gallery" className="object-cover w-full h-full transform transition duration-500 hover:scale-110" />
+                <div key={image.public_id} className="image-container overflow-hidden mb-4">
+                    <img src={image.url} alt="Gallery" className="object-cover max-w-full h-auto max-h-full" />
+                    {/* om jag skulle vilja att bilderna är i en storlek */}
+                    {/* className="object-cover max-w-full h-auto max-h-full md:max-w-lg lg:max-w-xl" */}
                 </div>
             ))}
         </div>
